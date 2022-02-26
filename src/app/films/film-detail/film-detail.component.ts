a3eac346bc1297a3b4a71746b528720435a00fe8
lucasgazzola <lucasgazzola1@outlook.com>
2022-02-26 17:05:39 -0300

    create the component

--- src/app/films/film-detail/film-detail.component.ts
+++ src/app/films/film-detail/film-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Film } from 'src/app/interfaces/interfaces';
 
 @Component({
@@ -11,17 +12,17 @@ import { Film } from 'src/app/interfaces/interfaces';
 
 export class FilmDetailComponent implements OnInit {
 
-  @Input() films: Film[] = [];
+//   films!: Observable<Film[]>;
   
-  id:number | undefined;
-  constructor(private route: ActivatedRoute) { }
+//   id:number | undefined;
+//   constructor(private route: ActivatedRoute, private films$: Observable<Film[]>) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
-      this.id = params["id"];
-    });
+//     this.route.params.subscribe((params) => {
+//       this.id = params["id"];
+//     });
 
-    console.log(this.id)
-    console.log(this.films)
+//     console.log(this.id)
+//     console.log(this.films)
   }
 }
