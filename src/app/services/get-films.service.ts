76b784963f74c2d797fecf4239b62cde6a271b58
lucasgazzola <lucasgazzola1@outlook.com>
2022-02-26 17:05:14 -0300

    reconstruction

--- src/app/services/get-films.service.ts
+++ src/app/services/get-films.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.prod';
-import { map, Observable, tap } from 'rxjs';
+import { BehaviorSubject, tap, Observable } from 'rxjs';
 import { Film, Result } from '../interfaces/interfaces';
 
 @Injectable({
@@ -10,15 +10,25 @@ import { Film, Result } from '../interfaces/interfaces';
 
 export class GetFilmsService {
   private apiURL = `${environment.API_URL}?api_key=${environment.API_KEY}&query=`
+  private _films = new BehaviorSubject<Film[]>([]);
+
+  readonly films = this._films.asObservable();
   
   constructor(private http: HttpClient) { }
 
-  getFilms(search: string): Observable<Film[]> {
-    return this.http.get<Result>(this.apiURL+search)
-    .pipe(
-      map(item => item.results),
-      tap(_ => console.log("fetched films"))
-    );
+  updateFilmSearch(search: string): void {
+    this.http.get<Result>(this.apiURL+search)
+    .subscribe({
+      next: (data) => {
+        this._films.next(data.results);
+      },
+      error : (error) => {
+        console.log('Could not load films: ', error);
+    }});
+  }
+  
+  get filmsList(): Observable<Film[]>{
+    return this.films;
   }
 
 }
