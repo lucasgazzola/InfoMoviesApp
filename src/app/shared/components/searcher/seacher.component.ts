212bb18cd4763affcc0e3dd458e42d810d1aa3f1
lucasgazzola <lucasgazzola1@outlook.com>
2022-02-27 20:44:57 -0300

    create searcher component as shared

---
+++ src/app/shared/components/searcher/seacher.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GetFilmsService } from 'src/app/services/get-films.service';
+
+@Component({
+  selector: 'app-searcher',
+  templateUrl: './searcher.component.html',
+  styleUrls: ['./searcher.component.scss']
+})
+export class SearcherComponent implements OnInit {
+  search: string = '';
+  lastSearch!: string;
+  constructor(private getFilmsService: GetFilmsService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.getFilmsService.lastSearchValue.subscribe(search => this.lastSearch = search);
+  }
+
+  handleSubmit(): void {
+    this.getFilmsService.updateFilmSearch(this.search);
+    this.router.navigate(['/films']);
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+    this.search = '';
+  }
+
+}
