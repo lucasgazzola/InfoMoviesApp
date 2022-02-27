e8e1128b2206c958cadeb99b9143cde86f8b1eed
lucasgazzola <lucasgazzola1@outlook.com>
2022-02-27 12:59:46 -0300

    add a last search subject to observe

--- src/app/services/get-films.service.ts
+++ src/app/services/get-films.service.ts
@@ -11,8 +11,10 @@ import { Film, Result } from '../interfaces/interfaces';
 export class GetFilmsService {
   private apiURL = `${environment.API_URL}?api_key=${environment.API_KEY}&query=`
   private _films = new BehaviorSubject<Film[]>([]);
+  private _lastSearch = new BehaviorSubject<string>('');
 
   readonly films = this._films.asObservable();
+  readonly lastSearch = this._lastSearch.asObservable();
   
   constructor(private http: HttpClient) { }
 
@@ -21,15 +23,20 @@ export class GetFilmsService {
     .subscribe({
       next: (data) => {
         this._films.next(data.results);
+        this._lastSearch.next(search);
       },
       error : (error) => {
         console.log('Could not load films: ', error);
     }});
+    
   }
   
   get filmsList(): Observable<Film[]>{
     return this.films;
   }
 
+  get lastSearchValue(): Observable<string>{
+    return this.lastSearch;
+  }
 }
 
