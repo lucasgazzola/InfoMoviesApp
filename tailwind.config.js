33a3fa4d0b956510ab96034c518e3621b60bc10e
lucasgazzola <lucasgazzola1@outlook.com>
2022-03-01 19:42:28 -0300

    fix bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,13 @@
 module.exports = {
-  purge: [],
+  
   darkMode: false, // or 'media' or 'class'
   prefix: "",
-  purge: {
-    enabled: true,
-    content: ["./src/**/*.{html,ts}"],
-  },
+  content: ["./src/**/*.{html,ts}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     minWidth: {
       '250': '250px',
     },
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
