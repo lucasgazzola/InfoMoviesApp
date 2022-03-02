2b934e31c08d243809fd3ae9feeb2278378f1ef9
lucasgazzola <lucasgazzola1@outlook.com>
2022-03-02 13:29:26 -0300

    add some logic to reach the top of the page

--- src/app/pages/films/components/films/films.component.ts
+++ src/app/pages/films/components/films/films.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faArrowAltCircleUp } from '@fortawesome/free-solid-svg-icons';
+import { fromEvent, Subscription } from 'rxjs';
 
 import { Film } from 'src/app/interfaces/interfaces';
 
@@ -15,13 +17,25 @@ export class FilmsComponent implements OnInit {
   search: string = '';
   films$!: Film[];
   lastSearch!: string;
-  
-  
+  visibility: 'hidden' | 'visible' = 'hidden';
+  scroller!: Subscription;
+  icon = faArrowAltCircleUp;
+   
   constructor(private getFilmsService: GetFilmsService) { }
 
   ngOnInit(): void {
+    this.scroller = fromEvent(window, 'scroll')
+      .subscribe(() => this.handleVisibility(window.scrollY));
     this.getFilmsService.lastSearchValue.subscribe(search => this.lastSearch = search);
     this.getFilmsService.getTrendingFilms();
     this.getFilmsService.films.subscribe(films => this.films$ = films);
   } 
+
+  handleGoTop():void{
+    window.scrollTo(0,0);
+  }
+
+  handleVisibility(y: number): void {
+    this.visibility = y > 400 ? 'visible' : 'hidden';
+  }
 }
